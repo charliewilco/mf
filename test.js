8a07db7f387714b2ce0488ad78ea1c62d45b6790
Charles ⚡ <charlespeters42@gmail.com>
2018-10-20 14:19:00 -0700

    prettier and new deps

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-const execa = require('execa')
-const fs = require('fs')
-const del = require('del')
-const MF = require('.')
+const execa = require("execa");
+const fs = require("fs");
+const del = require("del");
+const MF = require(".");
 
-describe('MF', () => {
-	const cli = execa('./cli.js', ['./dist/foo.js', './dist/bar.js', './dist/baz.js'])
+describe("MF", () => {
+  const cli = execa("./cli.js", ["./dist/foo.js", "./dist/bar.js", "./dist/baz.js"]);
 
-	it('runs and has standard out with emoji', () => {
-		cli.then(r => expect(r.stdout).toContain('👍'))
-	})
+  it("runs and has standard out with emoji", () => {
+    cli.then(r => expect(r.stdout).toContain("👍"));
+  });
 
-	it('produces files', () => {
-		fs.readdir('./dist', (err, files) => expect(files).toHaveLength(3))
-	})
+  it("produces files", () => {
+    fs.readdir("./dist", (err, files) => expect(files).toHaveLength(3));
+  });
 
-	afterAll(() => del(['dist/']))
-})
+  afterAll(() => del(["dist/"]));
+});
