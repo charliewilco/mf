2ae035778eb3bc051110335bd9a9b81173edf151
Charlie ⚡️ <charliewilc0@protonmail.com>
2022-10-30 18:08:46 +0000

    upgrading deps and removing yarn

--- test/make-file.test.js
+++ test/make-file.test.js
@@ -1,28 +1,28 @@
 // @ts-check
 import { execa } from "execa";
-import fs from "fs";
-import del from "del";
+import fs from "node:fs/promises";
+import { deleteAsync } from "del";
 
 const FILES = ["foo.js", "bar.js", "baz.js"];
 
 const LOCATION_FILES = FILES.map((f) => `./dist/${f}`);
 
 describe("makeFile", () => {
-  it("runs and has standard out with emoji", async () => {
-    try {
-      const result = await execa("./cli.js", LOCATION_FILES);
-      expect(result.stdout).toContain("🌈");
-      expect(result.stdout).toContain("👍");
-    } catch (err) {
-      console.log(err);
-    }
-  });
+	it("runs and has standard out with emoji", async () => {
+		try {
+			const result = await execa("./cli.js", LOCATION_FILES);
+			expect(result.stdout).toContain("🌈");
+			expect(result.stdout).toContain("👍");
+		} catch (err) {
+			console.log(err);
+		}
+	});
 
-  it("produces files", () => {
-    const files = fs.readdirSync("./dist");
+	test("produces files", async () => {
+		const files = await fs.readdir("./dist");
 
-    FILES.forEach((f) => expect(files).toContain(f));
-  });
+		FILES.forEach((f) => expect(files).toContain(f));
+	});
 
-  afterAll(() => del(LOCATION_FILES));
+	afterAll(async () => await deleteAsync(LOCATION_FILES));
 });
